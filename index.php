f6d3a510001e7e7e18561ae9d34e89032e28f9e7
Mariateb <sabmou05@gmail.com>
2023-02-20 22:14:04 +0100

    Test PHP

--- index.php
+++ index.php
@@ -5,7 +5,14 @@
     <body style="background-color: #000000">
         <div style="height: 40%">
         </div>
-        <div style="height: 20%; text-align: center;">
+        <div style="height: 5%; text-align: center;">
+            <p style="height: 100%; color: #ffffff">
+            <?php
+                echo "Heha"
+            ?>
+            </p>
+        </div>
+        <div style="height: 15%; text-align: center;">
             <img src="mariateb.png" style="height: 100%">
             <img src="tenshi.gif" style="height: 100%">
         </div>
